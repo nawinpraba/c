c9923fd2bdc41251ea3919a700b9f1c934b3f8dd
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-01-24 16:48:26 +0000

    Create .c
    c program

---
+++ .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("enter the value of a and b",a,b);
+scanf("%d",&a,&b);
+printf("additional",a+b);
+printf("multiplication",a*b);
+getch();
+}
